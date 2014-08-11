d64ee6d3da4692810a12954dcfc0ce020ea20f5d
Eric Kramer <ekramer@scripps.edu>
2014-08-11 10:17:31 -0700

    heatmaps

--- rfe/heatmaps.R
+++ rfe/heatmaps.R
@@ -1,3 +1,5 @@
+library("gplots")
+library("RColorBrewer")
 
 hm.simple <- function(d, main, f.names, cexCol=1){
   x = as.matrix(d[f.names])
@@ -5,7 +7,7 @@ hm.simple <- function(d, main, f.names, cexCol=1){
   
   heatmap.2(x,
             Rowv=T,
-            col=col, 
+            col=redgreen(75), 
             scale="row", 
             trace="none", 
             key=F, 
@@ -13,9 +15,23 @@ hm.simple <- function(d, main, f.names, cexCol=1){
             cexCol=cexCol, 
             cexRow=1,
             main=main,
-            labCol=as.character(d$Status),
-            dist=function(x) dist(x, method="manhattan"))
+            labCol=as.character(d$Status))
   
 }
 
 load("../data/rfe_data.Rdata")
+load("../data/cec.Rdata")
+
+d = genes[genes$SN > 100 | is.na(genes$SN), ]
+
+validation = d[d$Cohort == "VALIDATION", ]
+training = d[d$Cohort != "VALIDATION", ]
+f.names = gene.rfe$denovo$optVariables
+
+
+pdf("../figures/heatmaps.pdf", width=10)
+
+hm.simple(training, "Training Set", f.names)
+hm.simple(validation, "Validation Set", f.names)
+
+dev.off()
