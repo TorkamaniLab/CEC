62e44e16e1febb3160f05febf8cfcd519fde3eaa
Eric Kramer <ekramer@scripps.edu>
2014-08-12 21:34:01 -0700

    Small changes

--- linear/global_analysis.R
+++ linear/global_analysis.R
@@ -12,12 +12,21 @@ fc = sapply(x, function(z) median(z[y=="AMI"]) - median(z[y!="AMI"]))
 
 ## pca
 p = prcomp(as.matrix(x), center=T, scale.=T)
-s = p$rotation[,3]
+s = p$rotation[,1:3]
 
 fc.rnk = data.frame(Gene=names(fc), fc=fc)
-s.rnk = data.frame(Gene=names(s), s=s)
+
 
 
 write.rnk = function(x, f) write.table(x, quote=F, sep="\t", row.names=F, col.names=F, file=f)
 write.rnk(fc.rnk, "../data/GSEA/fc.rnk")
-write.rnk(s.rnk, "../data/GSEA/s.rnk")
+lapply(1:3, function(i){
+  s.rnk = data.frame(Gene=row.names(s), s=s[,1])
+  write.rnk(s.rnk, paste("../data/GSEA/s", i, ".rnk", sep=""))
+})
+
+load("~/Projects/CEC/data/cec.Rdata")
+ind = laply(genes[gene.names], 
+      function(g) which(laply(probes, function(p) all(g == p))), 
+      .progress="text")
+probe.names = colnames(probes)[ind]
