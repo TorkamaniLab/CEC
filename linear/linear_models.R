eee44ed0cce894fc941866b9883e444463d28a03
Eric Kramer <ekramer@scripps.edu>
2014-09-03 14:46:02 -0700

    cleaning up directory

--- linear/linear_models.R
+++ linear/linear_models.R
@@ -32,12 +32,11 @@ ind = sapply(x.t, function(z) median(z[y.t == "AMI"]) > 1 + median(z[y.t !="AMI"
 x.t = x.t[,ind]
 x.v = x.v[,ind]
 
-
 ## train lasso 
 m.lasso = cv.glmnet(as.matrix(x.t), y.t, family="binomial", pmax=5)
 p.t.lasso = predict(m.lasso, newx=as.matrix(x.t)) # predict on training set
 p.v.lasso = predict(m.lasso, newx=as.matrix(x.v)) # predict on validation set
-pROC::auc(roc(y.v, p.v.net))
+pROC::auc(roc(y.v, p.v.lasso))
 
 ## find selected genes
 co = coefficients(m.lasso)
@@ -55,6 +54,8 @@ genes.elastic = rownames(co)[co[,1] != 0] # find non-zero coefficients
 genes.elastic = genes.elastic[grep("Intercept", genes.elastic, invert=T)] # delete intercept term
 
 ## post-hoc fold changes
+qpcr = read.delim("../data/qpcr.txt")
+
 fc.lasso = data.frame(Gene=genes.lasso) %>%
   mutate(Coefficient=-co[genes.lasso,1]) %>%
   mutate(log.FC.discovery=sapply(x.t[genes.lasso], function(z) median(z[y.t=="AMI"]) - median(z[y.t!="AMI"]))) %>%
@@ -68,13 +69,12 @@ fc.elastic = data.frame(Gene=genes.elastic) %>%
   mutate(Coefficient=-co[genes.elastic,1]) %>%
   mutate(log.FC.discovery=sapply(x.t[genes.elastic], function(z) median(z[y.t=="AMI"]) - median(z[y.t!="AMI"]))) %>%
   mutate(log.FC.validation=sapply(x.v[genes.elastic], function(z) median(z[y.v=="AMI"]) - median(z[y.v!="AMI"]))) %>%
-  mutate(FC.discovery=2^log.FC.discovery) %>%
-  mutate(FC.validation=2^log.FC.validation) %>%
-  arrange(-Coefficient)
+  mutate(fc.discovery=2^log.FC.discovery) %>%
+  mutate(fc.validation=2^log.FC.validation) %>%
+  arrange(-Coefficient) %>%
 
-fc.elastic$Previous.qPCR = fc.elastic$Gene %in% colnames(qpcr)
+fc.elastic$Previous.qPCR = fc.elastic$gene %in% colnames(qpcr)
 
-qpcr = read.delim("../data/qpcr.txt")
 
 save(m.lasso, m.elastic, file="../data/linear_results_filtered.Rdata")
 
@@ -83,5 +83,3 @@ write.table(fc.lasso, file="../figures/5_gene_model.xls", sep="\t",
 
 write.table(fc.elastic, file="../figures/18_gene_model.xls", sep="\t",
             quote=F, row.names=F)
-
-
---
+++ linear/p_value_cal.R
@@ -0,0 +1,76 @@
+library("limma")
+
+load("~/Projects/CEC/data/cec_filtered2.Rdata")
+load("../data/linear_results_filtered.Rdata")
+
+x.t = genes[genes$Cohort != "VALIDATION", ] %>%
+  select(-Cohort, -SN, -Status, -Array)
+y.t = genes[genes$Cohort != "VALIDATION", ]$Status
+
+x.v = genes[genes$Cohort == "VALIDATION" & genes$SN > 100, ] %>%
+  select(-Cohort, -SN, -Status, -Array)
+y.v = genes[genes$Cohort == "VALIDATION" & genes$SN > 100, ]$Status
+
+lm.t = lmFit(t(x.t), model.matrix( ~ y.t))
+lm.v = lmFit(t(x.v), model.matrix( ~ y.v))
+
+elm.t = eBayes(lm.t)
+elm.v = eBayes(lm.v)
+
+df.t = topTable(elm.t, coef=2, number=ncol(x.t))
+df.v = topTable(elm.v, coef=2, number=ncol(x.v))
+
+p.vals = inner_join(
+  df.t %>% mutate(gene=row.names(df.t)),
+  df.v %>% mutate(gene=row.names(df.v)),
+  by="gene"
+  )
+colnames(p.vals) = gsub(".y$", ".validation", colnames(p.vals), perl=T)
+colnames(p.vals) = gsub(".x$", ".discovery", colnames(p.vals), perl=T)
+
+
+coefficients = m.elastic %>%
+  coef %>%
+  as.matrix %>%
+  as.data.frame
+coefficients$gene = row.names(coefficients)
+colnames(coefficients) = c("coef", "gene")
+
+df = inner_join(p.vals, coefficients) %>%
+  filter(coef != 0) %>%
+  arrange(coef) %>%
+  select(gene, coef, logFC.discovery, P.Value.discovery, adj.P.Val.discovery,
+         logFC.validation, P.Value.validation, adj.P.Val.validation) %>%
+  mutate(coef=-coef) %>%
+  mutate(logFC.validation=-logFC.validation) %>%
+  mutate(logFC.discovery=-logFC.discovery) %>%
+  mutate(previous.qpcr=gene %in% colnames(qpcr)) %>%
+  mutate(sig= adj.P.Val.discovery < 1e-3 & adj.P.Val.validation < 1e-3)
+
+write.table(df, quote=F, row.names=F, col.names=T, sep="\t",
+            file="~/Projects/CEC/figures/18_gene_model.xls")
+
+qpcr = read.delim("../data/qpcr.txt")
+
+qpcr.genes = df %>%
+  filter(sig) %>%
+  filter(previous.qpcr) %>%
+  select(gene) 
+
+qpcr2 = filter(qpcr, Used.in.Validation != "NO")
+
+y = qpcr2$Condition
+x = qpcr2[qpcr.genes$gene] %>% as.matrix
+x = apply(x, 2, function(y) y - qpcr2$GABPB1)
+
+m.qpcr = cv.glmnet(x, 
+                   y, 
+                   nfolds=nrow(x), 
+                   keep=T, 
+                   family="binomial",
+                   grouped=F,
+                   alpha=0.5)
+
+p.qpcr = m.qpcr$fit.preval[ , m.qpcr$lambda == m.qpcr$lambda.min]
+auc(roc(y, p.qpcr))
+
